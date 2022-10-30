8e163dd220562416fd7866fae93b9032d6c42f40
janeesh <janeesh.tin@gmail.com>
2022-10-30 23:52:08 +0530

    comparison operators js file

---
+++ comparison-operators.js
@@ -0,0 +1,21 @@
+var mybalance = 300;
+var nikeshoes = 79.99;
+
+if (nikeshoes <= mybalance) {
+  mybalance -= nikeshoes;
+  console.log("you bought new shoes")
+  console.log("Account Balance: " + mybalance)
+}
+
+
+// if the price of the shoes exceed the acc balance
+
+var mybalance = 300;
+var nikepremiumshoes = 799.99;
+
+if (nikeshoes <= mybalance) {
+  mybalance -= nikeshoes;
+  console.log("you bought new shoes")
+  console.log("Account Balance: " + mybalance)
+} else{console.log("insufficent balance");
+}
