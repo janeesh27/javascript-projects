2a6053b004ed594242f25785845435d2292dc7cd
janeesh <janeesh.tin@gmail.com>
2022-10-31 01:55:11 +0530

    arrays added

---
+++ arrays.js
@@ -0,0 +1,19 @@
+//  An array is a special variable, which can hold more than one value:
+//  Creating an Array
+//
+
+//  Syntax:
+
+//  var array_name = [item1, item2, ...];
+
+var students = ["vasu", "janeesh", "ayush"];
+var lowGrades = [];
+
+lowGrades.push(students[0]);   //pushing data in empty arrays
+
+console.log("total students:" + students);
+console.log("students with low grades:" + lowGrades);
+
+
+
+// pulling data out of an array
