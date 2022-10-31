018dad5f3d037b6ff4c6f6e3413caf6e935c4a09
janeesh <janeesh.tin@gmail.com>
2022-11-01 01:30:36 +0530

    objects and functions added

--- functions.js
+++ functions.js
@@ -3,6 +3,9 @@
 // A JavaScript function is a block of code designed to perform a particular task.
 // A JavaScript function is executed when "something" invokes it (calls it).
 
+// syntax function name(parameter1, parameter2, parameter3) {
+// code to be executed}
+
 // example 1
 //function to find area of a reactangle
 
---
+++ objects.js
@@ -0,0 +1,47 @@
+//JavaScript Objects
+//You have already learned that JavaScript variables are containers for data values.
+
+//Objects are variables too. But objects can contain many values.
+
+// syntax: var ObjectName = {
+//                 property1: value1,
+//                 property2: value2,
+//                 property3: value3,
+//                 property4: value4
+//                    };
+
+// example1
+
+var student1 = {
+  firstName: "ayush",
+  lastName: "gupta",
+  age: 22,
+};
+
+console.log(student1.firstName);
+console.log(student1.age);
+
+//example2 other ways to create a abject
+
+var student2 = new Object();
+student2.firstName = "vasu";   // way 2
+student2.lastName = "sharma";
+student2.age = 21;
+
+var student3 = {};
+student3.firstName = "janeesh";  // way3
+student3.lastName = "singh";
+student3.age = 23;
+
+
+// now to push this 3 students to an empty array
+
+var students=[];
+students.push(student1,student2,student3)
+
+console.log(students)
+
+
+// javascript objects with functions
+
+
