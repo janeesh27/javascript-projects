87de91ee6921e7d4fcd222353f821f2c57aabd27
janeesh <janeesh.tin@gmail.com>
2022-11-02 01:00:12 +0530

    class added

--- Fundamentals/7)class.js
+++ Fundamentals/7)class.js
@@ -2,3 +2,26 @@
 
 // JavaScript Classes are templlates for JavaScript Objects.
 
+// class ClassName {
+//  constructor() { ... }
+//
+//    }
+
+// example from https://www.w3schools.com/
+
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age(x) {
+    return x - this.year;
+  }
+}
+
+var date = new Date();
+var year = date.getFullYear();
+
+var myCar = new Car("Ford", 2014);
+
+console.log('my car is '+ myCar.age(year)+ ' years old')
