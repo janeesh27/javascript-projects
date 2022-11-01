a013edecf05b3fcbd4af0a65df80a4d541d0c6b9
janeesh <janeesh.tin@gmail.com>
2022-11-02 00:33:02 +0530

    completed percentage calculator

--- % Calculator/calculator.js
+++ % Calculator/calculator.js
@@ -1,5 +1,20 @@
 var num1 = document.getElementById("num1");
 var num2 = document.getElementById("num2");
-var result=document.getElementById("result");
+var result = document.getElementById("result");
 
-var form1=document.getElementById('form1')
+var form = document.getElementById("form1");
+
+form.addEventListener("submit", function (event) {
+  if (!num1.value || !num2.value) {
+    alert("please enter values in the feilds");
+  } else {
+    var x = parseFloat(num1.value);
+    var y = parseFloat(num2.value);
+
+    var z = x / y;
+    var percent = z * 100;
+
+    result.innerText = "Answer:" + percent + "%";
+    event.preventDefault();
+  }
+});
