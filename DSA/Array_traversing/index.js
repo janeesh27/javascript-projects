050b86e13d8510678215a8da491b8685e2830070
janeesh <janeesh.tin@gmail.com>
2022-11-09 01:27:02 +0530

    new

---
+++ DSA/Array_traversing/index.js
@@ -0,0 +1,7 @@
+var data = [1, 67, 77, 44, 65, 35, 2, 4, 6, 57];
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
+
+var x = 2;                     //accessing any element 
+console.log(data[x]);
