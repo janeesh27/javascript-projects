de42a3cbd8a3ab8394dc175e68ffee9fca735ef6
janeesh <janeesh.tin@gmail.com>
2022-11-12 00:25:24 +0530

    data types

---
+++ Fundamentals/9)data-types.js
@@ -0,0 +1,27 @@
+//  The Concept of Data Types
+//  In programming, data types is an important concept.
+
+//  To be able to operate on variables, it is important to know something about the type.
+//
+//
+//  Data types are dynamic
+//  JavaScript has dynamic types. This means that the same variable can be used to hold different data types:
+
+// example
+
+let x; // x is undefined
+x = 5; // x is a number
+x = "john"; // x is a string
+
+// JavaScript Booleans
+
+//  true or false
+
+// example
+
+let a = 5;
+let b = 5;
+let c = 7;
+
+console.log(a === b);
+console.log(a === c);
